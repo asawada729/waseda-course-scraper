827e02477043c0e8bf398186c170b863f19b8df0
Atsuki Sawada <asawada729@gmail.com>
2019-11-21 11:11:35 +0900

    Deleted unneeded files

--- src/json_to_pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open("course_data.json", "r") as json_file :
-    data = json.load(json_file)
-
-df = pd.DataFrame(data["course_list"])
-df.to_csv("course_list.csv")
